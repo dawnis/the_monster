4c941e5977b52715f4a634d76eb171ae72b5db60
GaiSensei <dawnis.chow@cornell.edu>
2022-10-26 21:55:49 -0400

    added factory method on creating polygon

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,51 @@ impl Monster {
     }
 }
 
+struct Polygon {
+    sides: usize,
+    centroid: (f32, f32),
+    scale: f32,
+    fill: Rgb<Srgb, u8>,
+    outline: Rgb<Srgb, u8>,
+}
+
+impl Polygon {
+    pub fn new(
+        sides: usize,
+        centroid: (f32, f32),
+        scale: f32,
+        fill: Rgb<Srgb, u8>,
+        outline: Rgb<Srgb, u8>,
+    ) -> Self {
+        Polygon {
+            sides,
+            centroid,
+            scale,
+            fill,
+            outline,
+        }
+    }
+
+    pub fn show(&self, draw: &Draw) {
+        assert!(self.sides <= 360);
+        let step = 360usize / self.sides;
+        let points = (0..=360).step_by(step.into()).map(|i| {
+            let radian = deg_to_rad(i as f32);
+            let x = radian.sin() * self.scale + self.centroid.0;
+            let y = radian.cos() * self.scale + self.centroid.1;
+            (pt2(x, y), self.fill)
+        });
+        draw.polygon().points_colored(points);
+        let points = (0..=360).step_by(step.into()).map(|i| {
+            let radian = deg_to_rad(i as f32);
+            let x = radian.sin() * self.scale + self.centroid.0;
+            let y = radian.cos() * self.scale + self.centroid.1;
+            (pt2(x, y), self.outline)
+        });
+        draw.polyline().weight(3.0).points_colored(points);
+    }
+}
+
 pub struct Head {
     pub color: Rgb<Srgb, u8>,
     pub outline: Rgb<Srgb, u8>,
@@ -30,21 +75,7 @@ pub struct Head {
 
 impl Rawr for Head {
     fn rawr(&self, d: &Draw) {
-        let points = (0..=360).step_by(1).map(|i| {
-            let radian = deg_to_rad(i as f32);
-            let x = radian.sin() * self.scale;
-            let y = radian.cos() * self.scale + self.scale * 2.0;
-            (pt2(x, y), self.color)
-        });
-        d.polygon().points_colored(points);
-
-        let points = (0..=360).step_by(1).map(|i| {
-            let radian = deg_to_rad(i as f32);
-            let x = radian.sin() * self.scale;
-            let y = radian.cos() * self.scale + self.scale * 2.0;
-            (pt2(x, y), self.outline)
-        });
-        d.polyline().weight(3.0).points_colored(points);
+        Polygon::new(360, (0.0, self.scale*2.0), self.scale, self.color, self.outline).show(d);
 
         d.ellipse()
             .x_y(0.0, self.scale * 2.0)
@@ -69,20 +100,7 @@ pub struct Body {
 
 impl Rawr for Body {
     fn rawr(&self, d: &Draw) {
-        let points = (0..=360).step_by(72).map(|i| {
-            let radian = deg_to_rad(i as f32);
-            let x = radian.sin() * self.scale + self.centroid.0;
-            let y = radian.cos() * self.scale + self.centroid.1;
-            (pt2(x, y), self.color)
-        });
-        d.polygon().points_colored(points);
-        let points = (0..=360).step_by(72).map(|i| {
-            let radian = deg_to_rad(i as f32);
-            let x = radian.sin() * self.scale + self.centroid.0;
-            let y = radian.cos() * self.scale + self.centroid.1;
-            (pt2(x, y), self.outline)
-        });
-        d.polyline().weight(3.0).points_colored(points);
+        Polygon::new(5, self.centroid, self.scale, self.color, self.outline).show(d);
     }
 }
 
