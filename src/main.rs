76a2c801d2bcf66c00d11c4861b88862f8303354
GaiSensei <dawnis.chow@cornell.edu>
2022-11-01 21:37:52 -0400

    switched to full app with monster movement

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,61 @@ mod monster;
 mod parts;
 use crate::monster::Monster;
 use nannou::prelude::*;
+use rand::Rng;
 
 fn main() {
-    nannou::sketch(view).run();
+    nannou::app(model).update(update).run()
 }
 
-fn view(app: &App, frame: Frame) {
+struct Model {
+    _window: window::Id,
+    pub monster_location: (f32, f32),
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window().view(view).build().unwrap();
+    Model {
+        _window,
+        monster_location: (0.0, 0.0),
+    }
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    model.monster_location = (
+        model.monster_location.0 + 0.1,
+        model.monster_location.1 + 0.1,
+    );
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     let outline_color = BLACK;
 
     draw.background().color(MEDIUMTURQUOISE);
+    let color_array = vec![
+        MEDIUMORCHID,
+        YELLOW,
+        ORANGE,
+        BLUE,
+        HOTPINK,
+        LAWNGREEN,
+        GOLD,
+        DEEPPINK,
+        CORAL,
+        AQUAMARINE,
+        CHOCOLATE,
+    ];
 
-    Monster::new((0.0, 0.0), 20.0, MEDIUMORCHID, outline_color).make(&draw);
+    let mut rng = rand::thread_rng();
+    let color_selection = rng.gen_range(0..color_array.len());
 
-    Monster::new((150.0, 150.0), 30.0, GOLD, outline_color).make(&draw);
+    Monster::new(
+        model.monster_location,
+        50.0,
+        color_array[color_selection],
+        outline_color,
+    )
+    .make(&draw);
 
-    Monster::new((-150.0, -150.0), 30.0, CHARTREUSE, outline_color).make(&draw);
-    Monster::new((150.0, -150.0), 30.0, HOTPINK, outline_color).make(&draw);
-    Monster::new((-150.0, 150.0), 30.0, INDIANRED, outline_color).make(&draw);
     draw.to_frame(app, &frame).unwrap();
 }
